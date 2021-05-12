f2a3368254bf241f670f934e963ec1fad834aa6b
ZehCoque <jose.coque@hotmail.com>
2021-05-12 13:52:40 -0300

    feat: query para alerta_final_contrato criada

--- server/models/contratos_query.js
+++ server/models/contratos_query.js
@@ -312,6 +312,31 @@ router.get('/contratos_alerta', function (req, res, next) {
     })
   });
 
+  router.get('/contratos_alerta_6_meses', function (req, res, next) {
+    auth.db_conn().getConnection((err,connection) => {
+
+      if (err) {
+        res.status(404).json((err));
+        return
+        }
+
+      let database = connection.config.database + '.';
+      connection.query(
+        'SELECT * FROM ' + database + 'alerta_final_contrato',
+        [],
+        (error, results) => {
+          if (error) {
+            console.log(error);
+            res.status(500).json({status: 'error'});
+          } else {
+            res.status(200).json(results);
+          }
+        }
+      );
+      connection.release();
+      })
+    });
+
 return router;
 }
 module.exports = contratos_router;
